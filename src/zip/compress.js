f82242b5a300fad07edb7db7fce8b313fdc48f85
DiffickMenLogo <gh.sdfs@mail.ru>
2022-12-05 17:54:47 +0300

    feat: make zip module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+
 const compress = async () => {
-    // Write your code here 
+  // implement function that compresses fileToCompress.txt to archive.gz using zlib and Streams API
+  const readStream = createReadStream("files/fileToCompress.txt");
+  const writeStream = createWriteStream("files/archive.gz");
+  pipeline(readStream, createGzip(), writeStream, (err) => {
+    if (err) {
+      console.error("Pipeline failed.", err);
+    } else {
+      console.log("Pipeline succeeded.");
+    }
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createUnzip } from "zlib";
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = createReadStream("files/archive.gz");
+  const writeStream = createWriteStream("files/fileToCompress.txt");
+  const unzip = createUnzip();
+  pipeline(readStream, unzip, writeStream, (err) => {
+    if (err) {
+      console.error("Pipeline failed.", err);
+    } else {
+      console.log("Pipeline succeeded.");
+    }
+  });
 };
 
 await decompress();
