b511031c64a1d1c2af5ac1c779d58922ba824785
DiffickMenLogo <gh.sdfs@mail.ru>
2022-12-05 12:09:04 +0300

    feat: make cli module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  //- implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't need to validate it) and prints them to the console in the format propName is value, prop2Name is value2
+  const args = process.argv;
+  const correctArgs = args.filter((arg) => arg.startsWith("--"));
+  const parsedArgs = correctArgs.map((arg) => {
+    const argIndex = args.indexOf(arg);
+    const value = args[argIndex + 1];
+    return `${arg.slice(2)} is ${value}`;
+  });
+  console.log(parsedArgs.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const env = process.env;
+  const rss = Object.keys(env)
+    .filter((key) => key.startsWith("RSS_"))
+    .map((key) => `${key}=${env[key]}`)
+    .join(", ");
+  console.log(rss);
 };
 
 parseEnv();
