3b8ceaddc2c3f58b7416a7d17e78531ba9fd1cc2
DiffickMenLogo <gh.sdfs@mail.ru>
2022-12-05 14:19:45 +0300

    feat: make streams module

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from "stream";
 const transform = async () => {
-    // Write your code here 
+  const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(chunk.toString().split("").reverse().join(""));
+      callback();
+    },
+  });
+  process.stdin.on("data", (data) => {
+    reverseStream.write(data);
+    reverseStream.end();
+    process.exit();
+  });
+  reverseStream.pipe(process.stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from "fs";
+
 const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream("files/fileToWrite.txt");
+  writeStream.on("error", (err) => {
+    console.log(err);
+  });
+  process.stdin.on("data", (data) => {
+    writeStream.write(data);
+    writeStream.close();
+    process.exit();
+  });
 };
 
 await write();
